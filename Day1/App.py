bb4aec791e09d3c2d7ba9e6942508cade6665bfb
Spritix <duval.kelian@gmail.com>
2024-12-04 15:19:00 +0100

    second star of the day

--- Day1/App.py
+++ Day1/App.py
@@ -1,20 +1,43 @@
+from collections.abc import list_iterator
+
 import Parsing_File
 
 list_sorted=[]
-list_result=[]
-somme = 0
 
-list_sorted = Parsing_File.parse_file("input.txt")
+list_gauche, list_droite = Parsing_File.parse_file("input.txt")
 
-for value in list_sorted:
-    if int(value[1]) > int(value[0]):
-        list_result.append(int(value[1])-int(value[0]))
-    else:
-        list_result.append(int(value[0])-int(value[1]))
+def day1_part1():
+    list_result=[]
+    result=0
+    while len(list_gauche)>0:
+        list_sorted.append([list_gauche.pop(0),list_droite.pop(0)])
+    for value in list_sorted:
+        if int(value[1]) > int(value[0]):
+            list_result.append(int(value[1])-int(value[0]))
+        else:
+            list_result.append(int(value[0])-int(value[1]))
 
-print(list_result)
+    print(list_result)
 
-for value in list_result:
-    somme += value
+    for value in list_result:
+        result = result + value
 
-print(somme)
+    print(result)
+
+def day1_part2():
+    list_iterator=[]
+    result=0
+    for value_gauche in list_gauche:
+        iterator = 0
+        for value_droite in list_droite:
+            if value_droite == value_gauche:
+                iterator += 1
+        list_iterator.append(iterator * int(value_gauche))
+    print(list_iterator)
+
+    for value in list_iterator:
+        result = result + int(value)
+
+    print(result)
+
+day1_part2()
--- Day1/Parsing_File.py
+++ Day1/Parsing_File.py
@@ -4,13 +4,10 @@ from unicodedata import numeric
 def parse_file(fileName):
     list_gauche=[]
     list_droite=[]
-    list_sorted = []
     f = open(fileName, 'r')
     for x in f:
         list_gauche.append(x.split("   ")[0])
         list_droite.append(x.split("   ")[1].removesuffix("\n"))
     list_gauche.sort()
     list_droite.sort()
-    while len(list_gauche)>0:
-        list_sorted.append([list_gauche.pop(0),list_droite.pop(0)])
-    return list_sorted
+    return list_gauche, list_droite
